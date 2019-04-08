513b0eebcbf109421acf2b0da4e16d318f51d2de
Naoki Maeda <maeda.naoki.md9@gmail.com>
2019-04-09 02:10:41 +0900

    Add webpack output config for Electron renderer
    Add webpack output config about renderer process

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path');
 
 const renderer = {
 	target: 'electron-renderer',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'renderer.js',
+	}
 }
 module.exports = {
   entry: './src/index.ts',
