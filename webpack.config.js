e75d9be1d668eca7424f73404161ce3445762cf2
Naoki Maeda <maeda.naoki.md9@gmail.com>
2019-04-14 01:40:06 +0900

    Set renderer bundle target to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,13 @@ const path = require('path');
 const renderer = {
 	target: 'electron-renderer',
 	entry: './src/renderer/renderer.js',
+	module: {
+		rules: [
+			{
+				test: /\.css/,
+			}
+		]
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'renderer.js',
