79f04e67217c8d9f2f28c456ce622cdabaff75fb
Naoki Maeda <maeda.naoki.md9@gmail.com>
2019-04-13 23:32:39 +0900

    Add renderer entry point for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const renderer = {
 	target: 'electron-renderer',
+	entry: './src/renderer/renderer.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'renderer.js',
