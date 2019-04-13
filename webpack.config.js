a44fd7d81ebe8f577a0ec5d1a5c045be1cceff28
Naoki Maeda <maeda.naoki.md9@gmail.com>
2019-04-14 02:02:57 +0900

    Untabify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path');
 
 const renderer = {
-	target: 'electron-renderer',
-	entry: './src/renderer/renderer.js',
-	module: {
-		rules: [
-			{
-				test: /\.css/,
-				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-			}
-		]
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'renderer.js',
-	}
+  target: 'electron-renderer',
+  entry: './src/renderer/renderer.js',
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'renderer.js',
+  }
 }
 
 module.exports = [
