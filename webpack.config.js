ef991b54729d8ae79f8130c20aae26ce22a1ae50
Naoki Maeda <maeda.naoki.md9@gmail.com>
2019-08-07 01:09:26 +0900

    Fix eslint & prettier error
    Convert single quote to double quote
    Add semicolon at the end of the sentence
    Change line break in module.export

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,46 @@
-const path = require('path');
+const path = require("path");
 
 const main = {
-  target: 'electron-main',
+  target: "electron-main",
   node: {
     __dirname: false,
     __filename: false
   },
-  entry: './src/main.ts',
-  devtool: 'inline-source-map',
+  entry: "./src/main.ts",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/
       }
     ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: [".tsx", ".ts", ".js"]
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
   }
 };
 
 const renderer = {
-  target: 'electron-renderer',
-  entry: './src/renderer/renderer.js',
+  target: "electron-renderer",
+  entry: "./src/renderer/renderer.js",
   module: {
     rules: [
       {
         test: /\.css/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       }
     ]
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'renderer.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "renderer.js"
   }
-}
+};
 
-
-module.exports = [
-  main,
-  renderer
-];
+module.exports = [main, renderer];
