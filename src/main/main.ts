392e9bfa02a0d6959c5c7d4738f07e8ababf3f3d
Naoki Maeda <maeda.naoki.md9@gmail.com>
2019-08-10 04:14:48 +0900

    Add basic main process file
    Add basic electron main process file

---
+++ src/main/main.ts
@@ -0,0 +1,48 @@
+import { app, BrowserWindow } from "electron";
+
+class App {
+  private mainWindow: BrowserWindow | null = null;
+  private app: Electron.App;
+  private filePath: string = `file://${__dirname}/../index.html`;
+
+  constructor(app: Electron.App) {
+    this.app = app;
+    this.app.on("ready", this.onReady.bind(this));
+    this.app.on("activate", this.onActivate.bind(this));
+    this.app.on("window-all-closed", this.onWindowAllClose.bind(this));
+  }
+
+  private onReady() {
+    this.createWindow();
+  }
+
+  private onActivate() {
+    if (this.mainWindow === null) {
+      this.createWindow();
+    }
+  }
+
+  private onWindowAllClose() {
+    if (process.platform !== "darwin") {
+      this.app.quit();
+    }
+  }
+
+  private createWindow() {
+    this.mainWindow = new BrowserWindow({
+      width: 800,
+      height: 400,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+
+    this.mainWindow.loadURL(this.filePath);
+
+    this.mainWindow.on("closed", () => {
+      this.mainWindow = null;
+    });
+  }
+}
+
+new App(app);
